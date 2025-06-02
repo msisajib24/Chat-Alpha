3459824dd2213aacc702eb43b6ba40f7cf9fb462
Alpha Development <msisajib857@gmail.com>
2025-06-02 09:36:21 +0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
 
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/chat_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://mjwrhxnqofgrvfslaeyl.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Alpha',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: Supabase.instance.client.auth.currentUser == null
+          ? const LoginScreen()
+          : const ChatScreen(),
+    );
+  }
+}
